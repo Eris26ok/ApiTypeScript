7c1d9bd92aa4d73bd7906946c54d8efef241d67a
Eris26ok <eris26ok@hotmail.com>
2022-02-22 11:50:32 -0300

    primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    return res.json({ message: 'hello world' });
+});
+
+app.listen(3333);
